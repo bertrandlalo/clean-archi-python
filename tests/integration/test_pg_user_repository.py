cf0e486220058427622a84c6352bacb146755d09
Raphaelle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-03-23 14:56:45 +0100

    Add postgres service in workflows

--- tests/integration/test_pg_user_repository.py
+++ tests/integration/test_pg_user_repository.py
@@ -1,5 +1,4 @@
 from sqlalchemy.engine import create_engine
-from sqlalchemy.sql import select
 from tests.utils.test_pg_url import test_pg_url
 
 from src.adapters.postgres.db import (
@@ -20,8 +19,10 @@ def prepare_db():
     initialize_db(engine)
     return engine
 
+
 patrice_user = User(first_name="patrice", last_name="bertrand", uuid="pat_uuid")
 anne_user = User(first_name="anne", last_name="bertrand-lalo", uuid="anne_uuid")
+
 
 def test_get_from_pg_repo():
     engine = prepare_db()
@@ -35,4 +36,3 @@ def test_get_from_pg_repo():
 
     assert repo.users == [patrice_user, anne_user]
     assert repo.get(uuid="pat_uuid") == patrice_user
-
