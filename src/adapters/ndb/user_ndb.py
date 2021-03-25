c9dc164a5573805a1c6a34ec31f7ae9ab746d7fd
Nathan Malnoury <nathan.malnoury@experquiz.com>
2021-03-25 19:17:30 +0100

    Refactor: ndb_user_repository.py

---
+++ src/adapters/ndb/user_ndb.py
@@ -0,0 +1,15 @@
+from google.cloud import ndb
+
+from domain.ports.model import User
+
+
+class UserNDB(ndb.Model):
+    first_name = ndb.StringProperty()
+    last_name = ndb.StringProperty()
+
+    @property
+    def id(self):
+        return self.key.to_legacy_urlsafe('h~').decode()
+
+    def to_user(self):
+        return User(first_name=self.first_name, last_name=self.last_name, uuid=self.id)
