eff2b4fc20ba1b2ccbb095b92d2f1db42afba5d7
Nathan Malnoury <nathan.malnoury@experquiz.com>
2021-03-25 19:19:50 +0100

    User: add method set_id and property id, to better miror ndb implementation

--- src/domain/ports/model.py
+++ src/domain/ports/model.py
@@ -1,9 +1,18 @@
 from __future__ import annotations
+
 from dataclasses import dataclass
+from typing import Any
 
 
 @dataclass
 class User:
     first_name: str
     last_name: str
-    uuid: str
+    uuid: Any = None
+
+    def set_id(self, id):
+        self.uuid = id
+
+    @property
+    def id(self):
+        return self.uuid
