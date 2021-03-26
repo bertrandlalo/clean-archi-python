e4468964286c3db392fc57e264a65080dcf45c4a
Nathan Malnoury <nathan.malnoury@experquiz.com>
2021-03-26 17:51:24 +0100

    Config: add optional middleware.

--- src/entrypoints/server.py
+++ src/entrypoints/server.py
@@ -3,10 +3,9 @@ from pathlib import Path
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-from adapters.csv_user_repository import CsvUserRepository
-from adapters.ndb.ndb_user_repository import NDBUserRepository
-from domain.ports.user_repository import AbstractUserRepository
 # configuration
+from adapters.csv_user_repository import CsvUserRepository
+from domain.ports.user_repository import AbstractUserRepository
 from domain.ports.uuid import AbstractUuid, RealUuid
 from domain.use_cases.create_new_user import CreateNewUser
 from entrypoints.config.config_local import LocalConfig
@@ -19,15 +18,19 @@ class Config:
 
     def __init__(self) -> None:
         self.user_repo = CsvUserRepository(csv_path=Path("data") / "user_repo.csv", uuid_generator=RealUuid())
+        self.has_middleware = False
+
+
 
 
 def make_app(config):
     # instantiate the app
     app = Flask(__name__)
+    if config.has_middleware:
+        app.wsgi_app = config.wsgi_middleware(wsgi_app=app.wsgi_app)
 
     # enable CORS
     CORS(app, resources={r"/*": {"origins": "*"}})
-
     # sanity check route
     @app.route("/ping", methods=["GET"])
     def ping_pong():
@@ -36,19 +39,20 @@ def make_app(config):
     # sanity check route
     @app.route("/users", methods=["GET"])
     def get_all_users():
-        return jsonify(config.user_repo.users)
+        return jsonify(users=config.user_repo.users)
 
     @app.route("/user", methods=["POST"])
     def add_new_user():
         # config.user_repo.add()
         json_data = request.get_json()
-        CreateNewUser(user_repository=config.user_repo, )
+        print(json_data)
+
+        CreateNewUser(user_repository=config.user_repo)
         return 'ok', 200
 
     return app
 
-
 if __name__ == "__main__":
-    config = LocalConfig()
-    app = make_app(config)
+    my_config = LocalConfig()
+    app = make_app(my_config)
     app.run()
