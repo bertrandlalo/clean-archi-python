6773618d4c336facb05df58d4138415ab716d123
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2021-04-08 16:28:56 +0200

    Config add port/host from env

--- tests/e2e/test_post_new_user.py
+++ tests/e2e/test_post_new_user.py
@@ -1,16 +1,19 @@
 from pathlib import Path
+from domain.models.events import EmailSentToWelcomeUser
+from tests.utils.spy_on_event import spy_on_event
 import pytest
 from flask import json
 from adapters.csv_topic_repository import CsvTopicRepository
 from adapters.csv_user_repository import CsvUserRepository
+from domain.ports.topic_repository import InMemoryTopicRepository
+from domain.ports.user_repository import InMemoryUserRepository
 
-from entrypoints.config import csv_config
 from entrypoints.config.config import Config
 from entrypoints.server import make_app
 
 config = Config(
-    user_repository=CsvUserRepository(csv_path=Path("data") / "user_repo"),
-    topic_repository=CsvTopicRepository(csv_path=Path("data") / "topic_repo"),
+    user_repository=InMemoryUserRepository(),
+    topic_repository=InMemoryTopicRepository(),
 )
 
 
@@ -23,5 +26,15 @@ def client():
 
 
 def test_post_new_user(client):
-    rv = client.post("/user", data={"name": "patrice", "status": "active"})
-    assert json.loads(rv.data) == "ok!"
+    published_events = spy_on_event(config.bus, EmailSentToWelcomeUser)
+    rv = client.post("/user", data={"name": "patrice"})
+    assert rv.status == "200 OK"
+    assert json.loads(rv.data) == {
+        "name": "patrice",
+    }
+    assert len(published_events) == 1
+
+
+def test_post_user_with_existing_name(client):
+    rv = client.post("/user", data={"name": "patrice"})
+    assert json.loads(rv.data) == {"name": "patrice", "status": "pending"}
